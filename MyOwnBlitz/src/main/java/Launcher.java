906c5e2b7c8d02d4e7509346606c3aaf7d900a47
lloyd westbury <lloydwestbury@gmail.com>
2022-05-29 18:30:01 +0200

    added ember and java project

---
+++ MyOwnBlitz/src/main/java/Launcher.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application{
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		
+		
+		
+		primaryStage.show();
+
+	
+	}
+
+	@Override
+	public void stop() throws Exception {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+	
+	
+
+}
